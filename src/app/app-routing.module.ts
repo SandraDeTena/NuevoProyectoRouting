9aa91b33cf44c69b358cb4c856c58800cc9c1aaa
Sandra <dtena90@gmail.com>
2021-05-10 15:23:36 +0200

    Rutas
    Hemos creado Rutas directas internas +Rutas dinámicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './contacto/contacto.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 import { InfoComponent } from './info/info.component';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: "home", component: MainComponent },
   { path: "info", component: InfoComponent },
-  { path: "contacto", component: ContactoComponent }
+  { path: "contacto", component: ContactoComponent },
+  { path: 'empleados/:empleadoId', component: EmpleadoComponent }, //Todo lo que haya depués de la barra, dices que es dinámica es decir: /:empleado
+  { path: '**', redirectTo: '/home' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Rutas';
+
+  // Hay que definir los parámetros del constructor. El primero hayq ue pasarle un objeto. Angular se ocupa arriba en este mismo documento de importarlo.
+  // constructor(router: Router){
+  // Si lo utilizamos dentro del CONSTRUCTOR - constructor(router: Router){}, solo se puede acceder desde el método del constructor.
+
+  //Si delante pones private - te permite utilizar desde cualquier punto de este documento. El objeto desde cualquier elemento -constructor(router: Router){}.
+  constructor(private router: Router) {
+
+  }
+
+  onClick(ruta) {
+
+    //Recuperación y envios de los datos previos a la navegación.
+    this.router.navigate([ruta]);
+
+  }
 }
